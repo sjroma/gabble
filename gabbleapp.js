e94f028369d0115bdb25a9b7dd4544ae06c20dcc
Steve Roma <sjroma@hotmail.com>
2017-07-14 17:22:23 -0400

    sign up and login ability added

--- gabbleapp.js
+++ gabbleapp.js
@@ -20,7 +20,7 @@ app.use(session({
 
 app.use(express.static('public'));
 app.use(expressValidator());
-app.use('/admin', adminRouter)
+//app.use('/admin', adminRouter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: false
@@ -31,7 +31,48 @@ app.get('/', function (req, res) {
 	res.render('login') //redirect so user is required to login to continue
 });
 
+app.post('/login', function(req,res) {
+	let user_name = req.body.user_name
+	let password = req.body.password
+	models.user.findOne({
+		where: {
+			user_name: user_name,
+			password: password
+		}
+	})
+	.then(function(user) {
+		if (user.password === password && user.user_name === user_name)
+			req.session.user_name = user_name;
+			req.session.userId = user.id;
+			req.session.authenticated = true;
+		  res.redirect('/index');
+	})
+	.catch(function(error) {
+		req.session.authenticated = false;
+		res.redirect('/login');
+	});	
+	return req.session;
+});
 
-app.listen(3000, function () {
+app.get('/signup', function(req,res) {
+	res.render('signup');
+});
+
+app.post('/signup-route', function(req,res) {
+	const user = models.user.build({
+		user_name: req.body.user_name,
+		password: req.body.password,
+		display_name: req.body.display_name
+	});
+	user.save();
+	res.render('login');
+});
+
+app.get('/index', function(req,res) {
+	res.render('index');
+});
+
+
+app.listen(3000, function() {
 	console.log('gabble application listening on port 3000')
 });
