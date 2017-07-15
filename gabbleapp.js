562264b62d88b13e84104041d51a7d106bc8144b
Steve Roma <sjroma@hotmail.com>
2017-07-14 21:35:04 -0400

    just prettied for easier reading

--- gabbleapp.js
+++ gabbleapp.js
@@ -31,34 +31,34 @@ app.get('/', function (req, res) {
 	res.render('login') //redirect so user is required to login to continue
 });
 
-app.post('/login', function(req,res) {
+app.post('/login', function (req, res) {
 	let user_name = req.body.user_name
 	let password = req.body.password
 	models.user.findOne({
-		where: {
-			user_name: user_name,
-			password: password
-		}
-	})
-	.then(function(user) {
-		if (user.password === password && user.user_name === user_name)
-			req.session.user_name = user_name;
+			where: {
+				user_name: user_name,
+				password: password
+			}
+		})
+		.then(function (user) {
+			if (user.password === password && user.user_name === user_name)
+				req.session.user_name = user_name;
 			req.session.userId = user.id;
 			req.session.authenticated = true;
-		  res.redirect('/index');
-	})
-	.catch(function(error) {
-		req.session.authenticated = false;
-		res.redirect('/login');
-	});	
+			res.redirect('/index');
+		})
+		.catch(function (error) {
+			req.session.authenticated = false;
+			res.redirect('/login');
+		});
 	return req.session;
 });
 
-app.get('/signup', function(req,res) {
+app.get('/signup', function (req, res) {
 	res.render('signup');
 });
 
-app.post('/signup-route', function(req,res) {
+app.post('/signup-route', function (req, res) {
 	const user = models.user.build({
 		user_name: req.body.user_name,
 		password: req.body.password,
@@ -68,11 +68,11 @@ app.post('/signup-route', function(req,res) {
 	res.render('login');
 });
 
-app.get('/index', function(req,res) {
+app.get('/index', function (req, res) {
 	res.render('index');
 });
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
 	console.log('gabble application listening on port 3000')
 });
